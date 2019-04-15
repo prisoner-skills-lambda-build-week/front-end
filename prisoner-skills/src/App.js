11354d9ec6b0584fc549824f35892e217521d11d
Garrett Weems <gwgraphicdesign@gmail.com>
2019-04-15 19:41:47 -0400

    navigation

--- prisoner-skills/src/App.js
+++ prisoner-skills/src/App.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react'
 import Register from './components/auth/register'
 import Prisons from './components/prisons'
 import Prisoners from './components/prisoners'
+import Navigation from './components/navigation'
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <header className='App-header'>
-          <Register />
-          <Prisons />
-          <Prisoners />
-        </header>
+        <Navigation />
+        <Register />
+        <Prisons />
+        <Prisoners />
       </div>
     )
   }
