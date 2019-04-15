4c7d4774900e84d96320dfe81f602d081a6f4768
Garrett Weems <gwgraphicdesign@gmail.com>
2019-04-15 18:06:08 -0400

    basic components

--- prisoner-skills/src/App.js
+++ prisoner-skills/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
+import Register from './components/auth/register'
+import Prisons from './components/prisons'
+import Prisoners from './components/prisoners'
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <header className='App-header'>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <Register />
+          <Prisons />
+          <Prisoners />
         </header>
       </div>
     )
